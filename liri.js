d6e92ec76a813502f28751d3ad0d38fb822c8bad
Jeffrey Layfield <jlayfield85@gmail.com>
2019-08-04 16:46:03 -0400

    updating liri.js

--- liri.js
+++ liri.js
@@ -1,3 +1,43 @@
 require("dotenv").config();
 var keys = require("./keys.js");
 var spotify = new Spotify(keys.spotify);
+var spotify = require('spotify');
+ 
+var getArtistNames = function(artist){
+    return artist.name
+}
+var getMeSpotify = function(songName) {
+
+
+spotify.search({ type: 'track', query: songName }, function(err, data) {
+    if ( err ) {
+        console.log('Error occurred: ' + err);
+        return;
+    }
+    
+    var songs = data.tracks.items;
+    for (var i=0; i<songs.length; i++){
+        console.log(i);
+        console.log('artist(s): ') + songs[i].artists.map(getArtistsNames);
+        console.log('song name: ' + songs[i].name);
+        console.log('preview.song: '+ songs[i].preview_url);
+        console.log('album: ' + songs[i].album.name);
+        console.log('--------------------------------------';)
+    }
+
+
+
+    var pick = function(caseData, functionData) {
+    switch(caseData) {
+        case 'spotify-this-song':
+            getMeSpotify(functionData);
+                break;
+            default:
+        console.log('LIRI does not know that');            
+    }
+}
+
+});
+}
+
+runThis(process.argv[2], process.argv[3]);
